592f9cd7e517bb9a65cf0e44e1ab1623e960b9b9
FF-SmartWork2 <fabioferreccio@hotmail.com>
2026-04-10 22:02:33 -0300

    fix: layout

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert'
 import path from 'path';
 
 export default defineConfig({
   server: {
+    https: true,
     allowedHosts: true,
     host: true,
     fs: {
       allow: ['..'], // Allow access to /src from /demo
     },
   },
+  plugins: [mkcert()]
 });
